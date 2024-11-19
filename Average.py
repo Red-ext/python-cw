471f7c1b0bf059c2cb53c2e889999ff131f7f70f
Red-ext <prustytwinkle@gmail.com>
2024-11-19 16:22:45 +0530

    Add files via upload
    Signed-off-by: Red-ext <prustytwinkle@gmail.com>

---
+++ Average.py
@@ -0,0 +1,11 @@
+num1=1
+num2=2
+num3=3
+num4=4
+num5=5
+
+addition=num1+num2+num3+num4+num5
+print("This is the sum of all of these numbers=", addition)
+
+average=addition/5
+print("This is the average of all of these numbers=", average)
