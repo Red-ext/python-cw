126f4584fcab28777afbf8434d50c8505462c2a3
Red-ext <prustytwinkle@gmail.com>
2024-11-21 17:20:57 +0530

    Add files via upload
    Signed-off-by: Red-ext <prustytwinkle@gmail.com>

--- Square Root.py
+++ Square Root.py
@@ -1,7 +1,5 @@
-squareroot=int(input("Please enter a square root"))
-
-num1=squareroot//625
-num2=(squareroot%625)//25
-
-print("25 is square root of 625", num1)
-print("625 is 25*25", num2)
+import math
+number = 16
+# Calculate square root
+square_root = math.sqrt(number)
+print("The square root of", number, "is", square_root)
